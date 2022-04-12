7e8257c27f6fc3d392a9190d43792d6137106a53
ShimKiYoung <shim8934@gmail.com>
2022-04-12 18:02:39 +0900

    1주차 2번 다음 큰 숫자 풀이 Commit

--- src/1주차/다음 큰 숫자/Solution.java
+++ src/1주차/다음 큰 숫자/Solution.java
@@ -1,6 +1,20 @@
 public class Solution {
     public int solution(int n) {
         int answer = 0;
+
+        String originBinary = Integer.toBinaryString(n);
+
+        int tempNum = n; // 임시변수
+
+        int num_1 = originBinary.length() - originBinary.replace("1","").length();
+        int num_2 = 0;
+
+        while(num_2 != num_1) {
+            tempNum++;
+            num_2 = Integer.toBinaryString(tempNum).length() - Integer.toBinaryString(tempNum).replace("1", "").length();
+            answer = tempNum;
+        }
+
         return answer;
     }
 }
