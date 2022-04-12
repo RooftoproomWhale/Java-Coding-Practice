35194946517f556b5d33a50919c49a6c569acdf7
ShimKiYoung <shim8934@gmail.com>
2022-04-13 05:59:04 +0900

    1-2 Integer 클래스의 bitCount 메소드 사용 풀이. 직접 구하는 방식 재작성 예정

--- src/1주차/다음 큰 숫자/Solution.java
+++ src/1주차/다음 큰 숫자/Solution.java
@@ -1,18 +1,13 @@
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = n;
 
-        String originBinary = Integer.toBinaryString(n);
-
-        int tempNum = n; // 임시변수
-
-        int num_1 = originBinary.length() - originBinary.replace("1","").length();
+        int num_1 = Integer.bitCount(n);
         int num_2 = 0;
 
-        while(num_2 != num_1) {
-            tempNum++;
-            num_2 = Integer.toBinaryString(tempNum).length() - Integer.toBinaryString(tempNum).replace("1", "").length();
-            answer = tempNum;
+        while (num_1 != num_2) {
+            answer++;
+            num_2 = Integer.bitCount(answer);
         }
 
         return answer;
