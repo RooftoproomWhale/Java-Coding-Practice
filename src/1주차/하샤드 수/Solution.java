ef827ad5a401f07e05625f47c28d94d1941c953e
ShimKiYoung <shim8934@gmail.com>
2022-04-12 15:29:05 +0900

    1. 하샤드 수 풀이 Commit

--- src/1주차/하샤드 수/Solution.java
+++ src/1주차/하샤드 수/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
+
+        int total = 0; // 자리수 합치기용 변수
+
+        // 1. 자리수 구하기 및 합치기
+        String[] temp = String.valueOf(x).split("");
+        for (String s : temp) {
+            total += Integer.parseInt(s);
+        }
+
+        if (x % total != 0) {
+            answer = false;
+        }
+
         return answer;
     }
 }
