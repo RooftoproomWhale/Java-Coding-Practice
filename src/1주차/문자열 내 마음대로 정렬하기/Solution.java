e2695e8a265336502494f9c7de29f7db3169f371
ShimKiYoung <shim8934@gmail.com>
2022-04-13 16:03:37 +0900

    1주차 3번 문자열 내 마음대로 정렬하기
    Arrays.sort 및 Comparator 오버라이딩으로 구현

--- src/1주차/문자열 내 마음대로 정렬하기/Solution.java
+++ src/1주차/문자열 내 마음대로 정렬하기/Solution.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
     public String[] solution(String[] strings, int n) {
-        return new String[] {};
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.charAt(n) != o2.charAt(n)) {
+                    return o1.substring(n).compareTo(o2.substring(n));
+                }
+                return o1.compareTo(o2);
+            }
+        });
+
+        return strings;
     }
 }
