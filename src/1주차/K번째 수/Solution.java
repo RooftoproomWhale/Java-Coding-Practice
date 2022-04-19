c2683f7cdf9945088eea9b2d19aa6b380488554a
ShimKiYoung <shim8934@gmail.com>
2022-04-20 01:33:51 +0900

    1주차 K번째 수 Arrays.copyOfRange 사용한 풀이 Commit

--- src/1주차/K번째 수/Solution.java
+++ src/1주차/K번째 수/Solution.java
@@ -5,17 +5,17 @@ public class Solution {
         int[] answer = new int[commands.length];
 
         for(int i = 0; i < commands.length; ++i){
-            int k = 0;
+            int[] answer = new int[commands.length];
 
-            int [] temp = new int[commands[i][1] - (commands[i][0]-1)]; // 2차원 배열의 길이로 정답배열의 길이
+            for(int i = 0; i < commands.length; ++i){
+                int [] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
 
-            for(int j = commands[i][0]-1; j <= commands[i][1]-1 ; ++j){
-                temp[k] = array[j];
-                k++;
+                Arrays.sort(temp);
+
+                answer[i] = temp[commands[i][2] - 1];
             }
 
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] -1];
+            return answer;
         }
 
         return answer;
