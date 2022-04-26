c3eb653e23afa27f1de8c2e4dcf928022bdeedf5
ShimKiYoung <shim8934@gmail.com>
2022-04-26 19:32:32 +0900

    2주 차 2번 올바른 괄호 String 배열과 카운트 변수 사용 풀이 Commit
    효율성 테스트에서 실패해서 다른 풀이 방법 탐색 중.
    아마... Stack을 이때 쓰면 적절하지 않을까 생각 중..

--- src/2주차/올바른 괄호/Solution.java
+++ src/2주차/올바른 괄호/Solution.java
@@ -1,6 +1,32 @@
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        return answer;
+    public boolean solution(String s) {
+        if (s.startsWith("(") && s.endsWith(")")) {
+            return checkCount(s.split(""));
+        }
+        else {
+            return false;
+        }
+    }
+
+    private boolean checkCount(String[] sArr) {
+        int count = 0;
+        for (String s : sArr) {
+            if (s.equals("(")) {
+                count++;
+            }
+            else if(count == 0) {
+                return false;
+            }
+            else {
+                count--;
+            }
+        }
+
+        if (count == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
