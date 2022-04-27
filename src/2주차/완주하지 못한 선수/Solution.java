4811ad39ca05639a6c6cf361ad967ab898137457
ShimKiYoung <shim8934@gmail.com>
2022-04-27 14:06:20 +0900

    2주 차 3번 완주하지 못한 선수 HashMap 사용 풀이 Commit

--- src/2주차/완주하지 못한 선수/Solution.java
+++ src/2주차/완주하지 못한 선수/Solution.java
@@ -1,5 +1,23 @@
 public class Solution {
     public String solution(String[] participant, String[] completion) {
-        return "";
+        String answer = "";
+
+        HashMap<String, Integer> complete = new HashMap<>();
+
+        for(String name : participant) {
+            complete.put(name, complete.getOrDefault(name, 0) + 1);
+        }
+        for (String name : completion) {
+            complete.put(name, complete.get(name) - 1);
+        }
+
+        for (String key : complete.keySet()) {
+            if (complete.get(key) > 0) {
+                answer = key;
+                break;
+            }
+        }
+
+        return answer;
     }
 }
