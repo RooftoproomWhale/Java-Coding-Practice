ae476491ba50ace2495bc9d7c361a72d4e9144ee
ShimKiYoung <shim8934@gmail.com>
2022-04-28 05:27:59 +0900

    2주 차 4번 위장 Map 사용 풀이 Commit
    PS. 경우의 수 공식이.......... 매우매우 기억이 안 났었음... 자괴감 든다...

--- src/2주차/위장/Solution.java
+++ src/2주차/위장/Solution.java
@@ -1,5 +1,23 @@
 public class Solution {
     public int solution(String[][] clothes) {
-        return 0;
+        int answer = 1;
+
+        HashMap<String, Integer> combination = new HashMap<>();
+
+        // 종류 가짓수 구하기
+        for(int i = 0; i<clothes.length; i++) {
+            String kinds = clothes[i][1];
+            combination.put(kinds, combination.getOrDefault(kinds,0)+1);
+        }
+
+        // 가짓수에 따른 경우의 수 모두 뽑기
+        for (String key : combination.keySet()) {
+            if (combination.get(key) > 0) {
+                answer *= (combination.get(key)+1);
+            }
+        }
+
+        // 모두 안 입는 경우의 수 1가지 제외
+        return answer-1;
     }
 }
