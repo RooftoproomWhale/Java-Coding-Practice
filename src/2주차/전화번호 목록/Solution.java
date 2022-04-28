19af2bc46b447c7418c743067e0b822d2e3f4419
ShimKiYoung <shim8934@gmail.com>
2022-04-28 10:15:39 +0900

    2주 차 5번 전화번호 목록 HashSet 사용 풀이 Commit
    효율성 테스트에서 실패한 부분이 있어 풀이방법 고민 중.
    
    예상 원인 1 -> 이중 포문을 사용하는 구조적인 문제의 TimeOver
    예상 원인 2 -> 비교하는 방식의 접근 방식을 바꿔야한다고 생각.

--- src/2주차/전화번호 목록/Solution.java
+++ src/2주차/전화번호 목록/Solution.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+
+        HashSet<String> phNum = new HashSet<>(Arrays.asList(phone_book));
+
+        for (String ph_number : phone_book) {
+            for (String num : phNum) {
+                if (ph_number.equals(num)== false && num.startsWith(ph_number)) {
+                    return false;
+                }
+            }
+        }
+
         return answer;
     }
 }
