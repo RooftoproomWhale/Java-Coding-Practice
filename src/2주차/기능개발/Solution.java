efbaae3539b29d69c4b3f988a96cda4d0f0888f1
ShimKiYoung <shim8934@gmail.com>
2022-04-30 02:26:39 +0900

    2주 차 1번 기능개발 - Math.ceil 함수 안 쓰고 수식으로 풀이... 의도한 게 이게 맞을 지는 모르겠네..

--- src/2주차/기능개발/Solution.java
+++ src/2주차/기능개발/Solution.java
@@ -1,50 +1,28 @@
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // 1. 리스트만 사용
-         List<Integer> completion = new ArrayList<>();
-         List<Integer> answer = new ArrayList<>();
+         List<Integer> answer = new ArrayList();
+         Queue<Integer> complete = new LinkedList();
 
-         for (int i = 0; i<progresses.length; i++) {
-         completion.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
+         for(int i = 0; i<progresses.length; i++) {
+              for (int j = 1; ; j++) {
+                   if ( (speeds[i] * j) >= (100 - progresses[i]) ) {
+                        complete.add(j);
+                        break;
+                   }
+              }
          }
 
-         int nth = 0;
-         while(nth != completion.size()) {
-         int daycnt = completion.get(nth);
-         int cnt = 1;
-         nth++;
+         while(!complete.isEmpty()) {
+              int daycnt = complete.poll();
+              int cnt = 1;
 
-         while(nth != completion.size() && daycnt >= completion.get(nth)) {
-         cnt++;
-         nth++;
-         }
-         answer.add(cnt);
+              while (!complete.isEmpty() && daycnt >= complete.peek()) {
+                   cnt++;
+                   complete.poll();
+              }
+              answer.add(cnt);
          }
 
          return answer.stream().mapToInt(Integer::intValue).toArray();
-
-
-         /*
-        // 2. Queue 사용
-        List<Integer> answer = new ArrayList();
-        Queue<Integer> complete = new LinkedList();
-
-        for(int i = 0; i<progresses.length; i++) {
-            complete.add( (int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
-        }
-
-        while(!complete.isEmpty()) {
-            int daycnt = complete.poll();
-            int cnt = 1;
-
-            while (!complete.isEmpty() && daycnt >= complete.peek()) {
-                cnt++;
-                complete.poll();
-            }
-            answer.add(cnt);
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-          */
     }
 }
