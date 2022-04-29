48470f421ca900e0ebe40db74f1566c90a05e585
ShimKiYoung <shim8934@gmail.com>
2022-04-29 20:11:50 +0900

    2주 차 4번 확장 포문 사용 Commit
    PS. getOrDefault 1로 사용할 경우 풀이 추가 실행 후 push 예정.

--- src/2주차/위장/Solution.java
+++ src/2주차/위장/Solution.java
@@ -4,10 +4,15 @@ public class Solution {
 
         HashMap<String, Integer> combination = new HashMap<>();
 
-        // 종류 가짓수 구하기
+        /* 종류 가짓수 구하기
         for(int i = 0; i<clothes.length; i++) {
             String kinds = clothes[i][1];
             combination.put(kinds, combination.getOrDefault(kinds,0)+1);
+        }
+         */
+
+        for (String[] s : clothes) {
+            combination.put(s[1], combination.getOrDefault(s[1],0)+1);
         }
 
         // 가짓수에 따른 경우의 수 모두 뽑기
